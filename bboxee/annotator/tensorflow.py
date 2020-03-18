18df77d668df5a6eed68e0b2cfd2c800489b5f16
Waylon Flinn <waylonflinn@gmail.com>
2020-03-18 15:39:20 -0500

    make tensorflow code slightly more general; make work with keras retinanet

--- bboxee/annotator/tensorflow.py
+++ bboxee/annotator/tensorflow.py
@@ -31,6 +31,36 @@ import tensorflow as tf
 import numpy as np
 
 
+
+# presets for 'caffe', 'tensorflow', 'pytorch'
+# 'caffe': subtract imagenet means, swap channels to BGR
+# 'tensorflow' : divide by 127.5, subtract 1
+# 'pytorch'
+
+# generic normalization (coreml style): global scale (multiply), per channel bias (add)
+
+
+# might also need channel format change NCHW <-> NHWC (channels first, channels last)
+
+
+# RGB means for imagenet
+imagenet_means = [123.68, 116.779, 103.939]
+
+# subtract of 123.68, 116.779, 103.939 and dividing by 58.393, 57.12, 57.375
+#https://stats.stackexchange.com/a/388461/26420
+
+def imagenet_normalize(images):
+    # assumes channels last?
+    images[..., 0] -= imagenet_means[0]
+    images[..., 1] -= imagenet_means[1]
+    images[..., 2] -= imagenet_means[2]
+
+    return images
+
+def rgb_to_bgr(images):
+    # assumes channels last?
+    return images[..., [2, 1, 0]]
+
 class Annotator(QtCore.QThread):
     """Threaded worker to keep gui from freezing while annotating images."""
 
@@ -47,6 +77,10 @@ class Annotator(QtCore.QThread):
         self.detection_graph = tf.Graph()
         self.inference_graph = inference_graph
         self.label_map = self.build_label_map(label_map)
+
+        # TODO: set these in GUI
+        self.imagenet_normalize = True
+        self.to_bgr = True
 
     def build_label_map(self, file_name):
         # see if we can use this to eliminated the need for
@@ -93,59 +127,118 @@ class Annotator(QtCore.QThread):
                 tf.import_graph_def(graph_def, name='')
             with tf.Session(graph=self.detection_graph) as sess:
                 # Definite input and output Tensors for detection_graph
+                # image_tensor = (self.detection_graph.
+                #                 get_tensor_by_name('image_tensor:0'))
+
+                #input_name = "image_tensor"
+                input_name = "input_1"
+
+                # input_1
                 image_tensor = (self.detection_graph.
-                                get_tensor_by_name('image_tensor:0'))
+                                get_tensor_by_name('{}:0'.format(input_name)))
+
+                # TODO: automate with 
+                # https://github.com/tensorflow/tensorflow/blob/master/tensorflow/tools/graph_transforms/README.md#inspecting-graphs
+                #output_names = ["detection_boxes", "detection_scores", "detection_classes", "num_detections"]
+                output_names = ["filtered_detections/map/TensorArrayStack/TensorArrayGatherV3",
+                                "filtered_detections/map/TensorArrayStack_1/TensorArrayGatherV3",
+                                "filtered_detections/map/TensorArrayStack_2/TensorArrayGatherV3"]
+
                 # Each box represents a part of the image where a
                 # particular object was detected.
+                output_name = output_names[0]
                 d_boxes = (self.detection_graph.
-                           get_tensor_by_name('detection_boxes:0'))
+                           get_tensor_by_name('{}:0'.format(output_name)))
+
                 # Each score represent how level of confidence for each of
                 # the objects. Score is shown on the result image,
                 # together with the class label.
+                output_name = output_names[1]
                 d_scores = (self.detection_graph.
-                            get_tensor_by_name('detection_scores:0'))
+                            get_tensor_by_name('{}:0'.format(output_name)))
+
+                output_name = output_names[2]
                 d_classes = (self.detection_graph.
-                             get_tensor_by_name('detection_classes:0'))
-                num_detections = (self.detection_graph.
-                                  get_tensor_by_name('num_detections:0'))
+                            get_tensor_by_name('{}:0'.format(output_name)))
+
+                if(len(output_names) > 3):
+                    output_name = output_names[3]
+                    num_detections = (self.detection_graph.
+                                get_tensor_by_name('{}:0'.format(output_name)))
+
+
                 for img in self.image_list:
                     file_name = os.path.join(self.image_directory, img)
                     image = Image.open(file_name)
                     # the array based representation of the image will be
                     # used later in order to prepare the result image with
                     # boxes and labels on it.
-                    image_np = np.array(image)
+                    image_np = np.array(image, dtype="float32")
+                    height, width, channels = image_np.shape
+
+                    if(self.imagenet_normalize):
+                        image_np = imagenet_normalize(image_np)
+
+                    if(self.to_bgr):
+                        image_np = rgb_to_bgr(image_np)
+
                     # Expand dimensions since the model expects images
-                    # to have shape: [1, None, None, 3]
+                    # to have shape: [1, None, None, 3] (channels last)
                     image_np_expanded = np.expand_dims(image_np, axis=0)
+
+
                     # Actual detection.
-                    fd = {image_tensor: image_np_expanded}
-                    (boxes, scores, classes, num) = sess.run([d_boxes,
-                                                              d_scores,
-                                                              d_classes,
-                                                              num_detections],
-                                                             feed_dict=fd)
+
+                    if(len(output_names) == 3):
+                        fd = {image_tensor: image_np_expanded}
+                        (boxes, scores, classes) = sess.run([d_boxes,
+                                                                  d_scores,
+                                                                  d_classes],
+                                                                 feed_dict=fd)
+                    elif(len(output_names) > 3):
+                        fd = {image_tensor: image_np_expanded}
+                        (boxes, scores, classes, num) = sess.run([d_boxes,
+                                                                  d_scores,
+                                                                  d_classes,
+                                                                  num_detections],
+                                                                 feed_dict=fd)
+
                     boxes = np.squeeze(boxes)
                     scores = np.squeeze(scores)
                     classes = np.squeeze(classes)
+
+                    #print(classes)
+                    #print(scores)
+
                     entry = schema.annotation_file_entry()
                     for i in range(len(scores)):
                         if scores[i] >= self.threshold:
                             annotation = schema.annotation()
                             annotation['created_by'] = 'machine'
                             bbox = boxes[i]
-                            annotation['bbox']['xmin'] = float(bbox[1])
-                            annotation['bbox']['xmax'] = float(bbox[3])
-                            annotation['bbox']['ymin'] = float(bbox[0])
-                            annotation['bbox']['ymax'] = float(bbox[2])
+                            # y first
+                            # annotation['bbox']['xmin'] = float(bbox[1])
+                            # annotation['bbox']['xmax'] = float(bbox[3])
+                            # annotation['bbox']['ymin'] = float(bbox[0])
+                            # annotation['bbox']['ymax'] = float(bbox[2])
+
+                            # x first
+                            annotation['bbox']['xmin'] = float(bbox[0]) / width
+                            annotation['bbox']['xmax'] = float(bbox[2]) / width
+                            annotation['bbox']['ymin'] = float(bbox[1]) / height
+                            annotation['bbox']['ymax'] = float(bbox[3]) / height
                             if classes[i] in self.label_map:
                                 label = self.label_map[classes[i]]
                             else:
                                 label = 'unknown'
                             # label = self.category_index[classes[i]]['name']
                             annotation['label'] = label
+
                             entry['annotations'].append(annotation)
                     if len(entry['annotations']) > 0:
+                        # order from top left to bottom right
+                        entry['annotations'] = sorted(entry['annotations'], key=lambda x : x['bbox']['xmin'])
+                        entry['annotations'] = sorted(entry['annotations'], key=lambda x : x['bbox']['ymin'])
                         self.data['images'][img] = entry
                     image.close()
                     counter += 1
