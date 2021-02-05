8f2f719572b4f422ac44b98625ceb6431b01aff6
pete <ersts@amnh.org>
2021-02-05 13:04:13 -0500

    Small updated based on feedback from CPW

--- converters/bbx2cpwpw.py
+++ converters/bbx2cpwpw.py
@@ -1,27 +1,3 @@
-# -*- coding: utf-8 -*-
-#
-# Bounding Box Editor and Exporter (BBoxEE)
-# Author: Peter Ersts (ersts@amnh.org)
-#
-# --------------------------------------------------------------------------
-#
-# This file is part of Animal Detection Network's (Andenet)
-# Bounding Box Editor and Exporter (BBoxEE)
-#
-# BBoxEE is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# BBoxEE is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this software.  If not, see <http://www.gnu.org/licenses/>.
-#
-# --------------------------------------------------------------------------
 import os
 import sys
 import json
@@ -51,11 +27,16 @@ except pyodbc.Error:
     print('Unable to open database.')
     sys.exit(0)
 
-# Load species list and ask for .bbx file
+# Load species list
 SPECIES = {}
 results = cur.execute('Select SpeciesID, CommonName from Species').fetchall()
 for rec in results:
     SPECIES[rec[1].lower()] = rec[0]
+if "none" not in SPECIES:
+    print('"None" label is missing from the species list')
+    sys.exit(0)
+
+# Ask for .bbx file
 BBX_FILE = input('Enter the path and file name of your .bbx file: ')
 BBX_FILE = os.path.abspath(BBX_FILE)
 IMAGE_PATH = os.path.dirname(BBX_FILE) + os.sep
@@ -67,21 +48,13 @@ except FileNotFoundError:
     print('Unable to open .bbx file.')
     sys.exit(0)
 
-# Get all image files
-files = glob.glob(IMAGE_PATH + '*')
-image_format = [".jpg", ".jpeg", ".png"]
-f = (lambda x: os.path.splitext(x)[1].lower() in image_format)
-image_list = list(filter(f, files))
-image_list = [os.path.basename(x) for x in image_list]
-image_list = sorted(image_list)
-
 # Check species labels match
 print('Verifing species names...')
 for image in DATA['images']:
     annotations = DATA['images'][image]['annotations']
     for a in annotations:
         if a['label'].lower() not in SPECIES:
-            print("Species list does not contain [{}]".format(a['label']))
+            print("Species list in the database does not contain [{}]".format(a['label']))
             sys.exit(0)
 print()
 
@@ -110,15 +83,24 @@ results = cur.execute('select LocationID, StudyAreaID, LocationName from CameraL
 for rec in results:
     locations[rec[0]] = (rec[1], rec[2])
 # Pull Visits data, VisitID, LocationID, VisitTypeID=2 (Pull) order by VisitDate
+visit_type = {1: 'Check', 2: 'Pull'}
 visit_id_list = []
-results = cur.execute('select VisitID, LocationID from Visits where VisitTypeID = 2 order by VisitDate asc').fetchall()
+results = cur.execute('select VisitID, LocationID, VisitDate, VisitTypeID from Visits where VisitTypeID = 2 or VisitTypeID = 1 order by VisitDate asc').fetchall()
 for rec in results:
     visit_id_list.append(str(rec[0]))
-    print('{}  {} - {}'.format(rec[0], study_area[locations[rec[1]][0]], locations[rec[1]][1]))
+    print('{}: {} - {} [{}] ({})'.format(rec[0], study_area[locations[rec[1]][0]], locations[rec[1]][1], rec[2], visit_type[rec[3]]))
 VISITID = input('Which VisitID should the data be associated with? ')
 if VISITID not in visit_id_list:
     print('That VisitID is not recognized')
     sys.exit(0)
+
+# Get all image files
+files = glob.glob(IMAGE_PATH + '*')
+image_format = [".jpg", ".jpeg", ".png"]
+f = (lambda x: os.path.splitext(x)[1].lower() in image_format)
+image_list = list(filter(f, files))
+image_list = [os.path.basename(x) for x in image_list]
+image_list = sorted(image_list)
 
 # Import data
 counter = 1
@@ -154,4 +136,6 @@ for name in tqdm(image_list):
 
         for d in detections.keys():
             cur.execute('INSERT INTO Detections (SpeciesID, Individuals, ObsID, ImageID) values (?, ?, ?, ?)', (SPECIES[d.lower()], detections[d], OBSID, image_rec_id))
-        conn.commit()
+    else:
+        cur.execute('INSERT INTO Detections (SpeciesID, Individuals, ObsID, ImageID) values (?, ?, ?, ?)', (SPECIES['none'], 0.0, OBSID, image_rec_id))
+    conn.commit()
